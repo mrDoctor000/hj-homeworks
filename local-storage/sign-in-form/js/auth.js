60bf8bb35f9df3de1843866e5a04b2a4a931202e
Egor Ponomarev <egor@ponomarev.pp.ru>
2017-11-12 20:18:17 +0300

    signIn/signUp

--- local-storage/sign-in-form/js/auth.js
+++ local-storage/sign-in-form/js/auth.js
@@ -2,7 +2,7 @@
 
 document.addEventListener('DOMContentLoaded', event => {
   const xhr = new XMLHttpRequest();
-  
+
   const signIn = document.querySelector('.sign-in-htm');
   const signUp = document.querySelector('.sign-up-htm');
 
@@ -10,28 +10,54 @@ document.addEventListener('DOMContentLoaded', event => {
     xhr.open('POST', 'https://neto-api.herokuapp.com/signin');
     xhr.setRequestHeader('Content-Type', 'application/json');
 
+    var user = {
+      'email': signIn.querySelector('#email').value,
+      'password': sihnIn.querySelector('#pass').value
+    };
 
-    xhr.send();
+    xhr.send(JSON.stringify(user));
 
-    if (xhr.status != 200) {
-        alert(xhr.status + ': ' + xhr.statusText);
-    } else {
-        alert(xhr.responseText);
-    }
+    xhr.addEventListener('load', event => {
+      var data = JSON.parse(xhr.responseText);
+
+      var mess = signIn.querySelector('.error-message')
+      if (data.error) {
+        return mess.textContent = data.message;
+      } else {
+        return mess.textContent = `Пользователь ${data.name} успешно авторизован`;
+      }
+    })
+
+
   });
 
   signUp.querySelector('.button').addEventListener('click', event => {
     xhr.open('POST', ' https://neto-api.herokuapp.com/signup');
     xhr.setRequestHeader('Content-Type', 'application/json');
-    
-        
-    xhr.send();
 
-    if (xhr.status != 200) {
-        alert(xhr.status + ': ' + xhr.statusText);
+    if (signUp.querySelector('[name="password"]').value === signUp.querySelector('[name="passwordcopy"]')) {
+      var user = {
+        'email': signUp.querySelector('#email').value,
+        'password': signUp.querySelector('#pass').value,
+        'name': signUp.querySelector('[name="name"]').value
+      };
     } else {
-        alert(xhr.responseText);
+      return mess.textContent = 'Пароль не совпадает';
     }
+
+    xhr.send(JSON.stringify(user));
+
+    xhr.addEventListener('load', event => {
+      var data = JSON.parse(xhr.responseText);
+
+      var mess = signIn.querySelector('.error-message')
+      if (data.error) {
+        mess.textContent = data.message;
+      } else {
+        mess.textContent = `Пользователь ${data.name} успешно зарегистрирован`;
+      }
+    })
+
   });
-  
+
 })
