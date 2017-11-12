fee7930a0667ee1ddb32b25a0edacd65a4362ad0
Егор Пономарев < >
2017-11-12 16:11:53 +0300

    first

--- local-storage/sign-in-form/js/auth.js
+++ local-storage/sign-in-form/js/auth.js
@@ -1,1 +1,37 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', event => {
+  const xhr = new XMLHttpRequest();
+  
+  const signIn = document.querySelector('.sign-in-htm');
+  const signUp = document.querySelector('.sign-up-htm');
+
+  signIn.querySelector('.button').addEventListener('click', event => {
+    xhr.open('POST', 'https://neto-api.herokuapp.com/signin');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+
+
+    xhr.send();
+
+    if (xhr.status != 200) {
+        alert(xhr.status + ': ' + xhr.statusText);
+    } else {
+        alert(xhr.responseText);
+    }
+  });
+
+  signUp.querySelector('.button').addEventListener('click', event => {
+    xhr.open('POST', ' https://neto-api.herokuapp.com/signup');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    
+        
+    xhr.send();
+
+    if (xhr.status != 200) {
+        alert(xhr.status + ': ' + xhr.statusText);
+    } else {
+        alert(xhr.responseText);
+    }
+  });
+  
+})
