dafd5a32106278f64b96ddacdf0f3a0d8538686f
Egor Ponomarev <egor@ponomarev.pp.ru>
2017-11-27 16:42:43 +0300

    first

--- cors/twitter/js/twitter.js
+++ cors/twitter/js/twitter.js
@@ -1,1 +1,24 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', event => {
+
+    fetch('https://neto-api.herokuapp.com/twitter/jsonp')
+    .then(res => {
+        console.log(res.headers.get('Content-Type'));
+        console.log(res.status);
+
+        return res.json();
+    })
+    .then(content => {
+        document.querySelector('[data-username]').innerHTML = `${content.username}`;
+        document.querySelector('[data-description]').innerHTML = `${content.description}`;
+        document.querySelector('[data-tweets]').innerHTML = `${content.tweets}`;
+        document.querySelector('[data-followers]').innerHTML = `${content.followers}`;
+        document.querySelector('[data-following]').innerHTML = `${content.following}`;
+
+        document.querySelector('[data-wallpaper]').src = content.wallpaper;
+        document.querySelector('[data-pic]').src = content.pic;
+    })
+
+})
+
