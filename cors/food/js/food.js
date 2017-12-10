e22f917cfe4beee20a8fd7af5656d0821456268f
Egor Ponomarev <egor@ponomarev.pp.ru>
2017-12-10 19:47:54 +0300

    cors

--- cors/food/js/food.js
+++ cors/food/js/food.js
@@ -3,91 +3,83 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', event => {
-    function loadFood(url) {
-        return new Promise((done, fail) => {
-            window.parseBook = done;
-            const script = document.createElement('script');
-            script.src = `${url}?jsonp=loadFood`;
-            document.body.appendChild(script);
-
-        })
+  function randName() {
+    var result = '';
+    var words = '0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM';
+    var max_position = words.length - 1;
+    for (var i = 0; i < 5; ++i) {
+      var position = Math.floor(Math.random() * max_position);
+      result = result + words.substring(position, position + 1);
     }
+    return result;
+  }
 
-    function loadRating(url) {
-        return new Promise((done, fail) => {
-            window.parseBook = done;
-            const script = document.createElement('script');
-            script.src = `${url}?loadRating`;
-            document.body.appendChild(script);
+  function loadFood(url) {
+    const funcName = randName();
+    return new Promise((done, fail) => {
+      window[funcName] = done;
 
+      const script = document.scripts[0].cloneNode();
+      script.src = `${url}?jsonp=${funcName}`;
+      document.body.appendChild(script);
+    })
+  }
+
+  loadFood('https://neto-api.herokuapp.com/food/42')
+    .then(res => {
+      return res.json();
+    })
+    .then(res => {
+      fetch('https://neto-api.herokuapp.com/food/42/rating')
+        .then(rat => {
+          console.log(rat.headers.get('Content-Type'));
+          console.log(rat.status);
+
+          return rat.json();
         })
-    }
-
-    function loadConsumers(url) {
-        return new Promise((done, fail) => {
-            window.parseBook = done;
-            const script = document.createElement('script');
-            script.src = `${url}?jsonp=loadConsumers`;
-            document.body.appendChild(script);
-
+        .then(rat => {
+          res.rating = rat;
         })
-    }
 
-    loadFood('https://neto-api.herokuapp.com/food/42')
-        .then(res => {
-            return res.json();
-        })
-        .then(res => {
-            fetch('https://neto-api.herokuapp.com/food/42/rating')
-                .then(rat => {
-                    console.log(rat.headers.get('Content-Type'));
-                    console.log(rat.status);
+      return res;
+    })
+    .then(res => {
+      fetch('https://neto-api.herokuapp.com/food/42/consumers')
+        .then(users => {
+          console.log(users.headers.get('Content-Type'));
+          console.log(users.status);
 
-                    return rat.json();
-                })
-                .then(rat => {
-                    res.rating = rat;
-                })
-
-            return res;
+          return users.json();
         })
-        .then(res => {
-            fetch('https://neto-api.herokuapp.com/food/42/consumers')
-                .then(users => {
-                    console.log(users.headers.get('Content-Type'));
-                    console.log(users.status);
-
-                    return users.json();
-                })
-                .then(users => {
-                    res.users = users;
-                })
-            return res;
+        .then(users => {
+          res.users = users;
         })
-        .then(res => {
-            document.querySelector('[data-pic]').style.backgroundImage = `url(${res.pic})`;
-            document.querySelector('[data-title]').textContent = res.title;
-            res.ingredients.forEach((el, i) => {
-                if (i === (res.ingredients.length - 1)) {
-                    document.querySelector('[data-ingredients]').textContent += `${el}.`;
-                } else {
-                    document.querySelector('[data-ingredients]').textContent += `${el}, `;
-                }
-            });
-            document.querySelector('[data-rating]').textContent = res.rating.rating.toFixed(2);
-            document.querySelector('[data-star]').width = `${res.rating * 10}%`;
-            document.querySelector('[data-votes]').textContent = `(${res.rating.votes} оценок)`;
-            res.users.list.forEach((el, i) => {
-                const img = document.createElement('img');
-                img.src = el.pic;
-                img.title = el.name;
-                document.querySelector('[data-consumers]').appendChild(img);
-                if(i === (res.users.list.length - 1)) {
-                    const span = document.createElement('span');
-                    span.textContent = `(+${res.users.total})`;
-                    document.querySelector('[data-consumers]').appendChild(span);
-                }
-            })
-        })
+      return res;
+    })
+    .then(res => {
+      document.querySelector('[data-pic]').style.backgroundImage = `url(${res.pic})`;
+      document.querySelector('[data-title]').textContent = res.title;
+      res.ingredients.forEach((el, i) => {
+        if (i === (res.ingredients.length - 1)) {
+          document.querySelector('[data-ingredients]').textContent += `${el}.`;
+        } else {
+          document.querySelector('[data-ingredients]').textContent += `${el}, `;
+        }
+      });
+      document.querySelector('[data-rating]').textContent = res.rating.rating.toFixed(2);
+      document.querySelector('[data-star]').width = `${res.rating * 10}%`;
+      document.querySelector('[data-votes]').textContent = `(${res.rating.votes} оценок)`;
+      res.users.list.forEach((el, i) => {
+        const img = document.createElement('img');
+        img.src = el.pic;
+        img.title = el.name;
+        document.querySelector('[data-consumers]').appendChild(img);
+        if (i === (res.users.list.length - 1)) {
+          const span = document.createElement('span');
+          span.textContent = `(+${res.users.total})`;
+          document.querySelector('[data-consumers]').appendChild(span);
+        }
+      })
+    })
 
 });
--- cors/profile/js/profile.js
+++ cors/profile/js/profile.js
@@ -1,57 +1,70 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', event => {
-    function loadProfile(url) {
-        return new Promise((done, fail) => {
-            window.parseBook = done;
-            const script = document.createElement('script');
-            script.src = `${url}?jsonp=loadProfile`;
-            document.body.appendChild(script);
-
-        })
+  function randName() {
+    var result = '';
+    var words = '0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM';
+    var max_position = words.length - 1;
+    for (var i = 0; i < 5; ++i) {
+      var position = Math.floor(Math.random() * max_position);
+      result = result + words.substring(position, position + 1);
     }
+    return result;
+  }
 
-    function loadTech(url) {
-        return new Promise((done, fail) => {
-            window.parseBook = done;
-            const script = document.createElement('script');
-            script.src = `${url}?jsonp=loadTech`;
-            document.body.appendChild(script);
+  function loadProfile(url) {
+    const funcName = randName();
+    return new Promise((done, fail) => {
+      window[funcName] = done;
 
-        })
-    }
+      const script = document.scripts[0].cloneNode();
+      script.src = `${url}?jsonp=${funcName}`;
+      document.body.appendChild(script);
+    })
+  }
 
-    loadProfile('https://neto-api.herokuapp.com/profile/me')
-        .then(res => {
-            return res.json();
-        })
-        .then(res => {
-            loadTech(`https://neto-api.herokuapp.com/profile/${res.id}/technologies`)
-                .then(tech => {
-                    return tech.json();
-                })
-                .then(tech => {
-                    res.technologies = tech;
-                });
+  function loadTech(url) {
+    const funcName = randName();
+    return new Promise((done, fail) => {
+      window[funcName] = done;
 
-            return res;
+      const script = document.scripts[0].cloneNode();
+      script.src = `${url}?jsonp=${funcName}`;
+      document.body.appendChild(script);
+    })
+  }
+
+  loadProfile('https://neto-api.herokuapp.com/profile/me')
+    .then(res => {
+      return res.json();
+    })
+    .then(res => {
+      loadTech(`https://neto-api.herokuapp.com/profile/${res.id}/technologies`)
+        .then(tech => {
+          return tech.json();
         })
-        .then(user => {
-            document.querySelector('[data-name]').textContent = user.name;
-            document.querySelector('[data-description]').textContent = user.description;
-            document.querySelector('[data-pic]').setAttribute('src', user.pic);
-            document.querySelector('[data-position]').textContent = user.position;
-            user.tech.forEach(el => {
-                const span = document.createElement('span');
-                span.className = `devicons devicons-${el}`;
-                document.querySelector('[data-technologies]').appendChild(span);
-            });
-        })
-        .then(() => {
-            document.querySelector('.content').style.display = 'initial';
-        })
-        .catch(err => {
-            console.error(err);
+        .then(tech => {
+          res.technologies = tech;
         });
+
+      return res;
+    })
+    .then(user => {
+      document.querySelector('[data-name]').textContent = user.name;
+      document.querySelector('[data-description]').textContent = user.description;
+      document.querySelector('[data-pic]').setAttribute('src', user.pic);
+      document.querySelector('[data-position]').textContent = user.position;
+      user.tech.forEach(el => {
+        const span = document.createElement('span');
+        span.className = `devicons devicons-${el}`;
+        document.querySelector('[data-technologies]').appendChild(span);
+      });
+    })
+    .then(() => {
+      document.querySelector('.content').style.display = 'initial';
+    })
+    .catch(err => {
+      console.error(err);
+    });
 
 });
